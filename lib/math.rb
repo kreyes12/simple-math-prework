742df5acdfe4aef8a79ee741b49a8207c6c426df
Karla R <karla.y.reyes12@gmail.com>
2018-07-08 15:26:56 +0000

    automatically backed up by learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,13 +1,13 @@
 def addition(num1, num2)
-  num1 + num2
+  return num1 + num2
 end
 
 def subtraction(num1, num2)
-  num1 - num2
+  return num1 - num2
 end
 
 def division(num1, num2)
-  num1 / num2
+  return num1.to_f / num2.to_f
 end
 
 def multiplication(num1, num2)
